32189374e12f4f9a91b1ba2a1c356ca4b17f00d2
Dana Franklin <realityendshere@users.noreply.github.com>
2019-08-18 22:17:35 -0700

    [EMBER] Upgrade to Ember v3.12.0
    * Upgrade to Ember v3.12.0
    * Run code mods
    * Update dependencies
    * Fix Mirage config
    * Lint fixes

--- tests/integration/components/loading-indicator-test.js
+++ tests/integration/components/loading-indicator-test.js
@@ -1,24 +1,26 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('loading-indicator', 'Integration | Component | loading indicator', {
-  integration: true
-});
+module('Integration | Component | loading indicator', function(hooks) {
+  setupRenderingTest(hooks);
 
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+  test('it renders', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{loading-indicator}}`);
+    await render(hbs`{{loading-indicator}}`);
 
-  assert.equal(this.$().text().trim(), '');
+    assert.dom('*').hasText('');
 
-  // Template block usage:
-  this.render(hbs`
-    {{#loading-indicator}}
-      template block text
-    {{/loading-indicator}}
-  `);
+    // Template block usage:
+    await render(hbs`
+      {{#loading-indicator}}
+        template block text
+      {{/loading-indicator}}
+    `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+    assert.dom('*').hasText('template block text');
+  });
 });
--- tests/unit/routes/no-help-test.js
+++ tests/unit/routes/no-help-test.js
@@ -1,11 +1,11 @@
-import { moduleFor, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
 
-moduleFor('route:no-help', 'Unit | Route | no help', {
-  // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
-});
+module('Unit | Route | no help', function(hooks) {
+  setupTest(hooks);
 
-test('it exists', function(assert) {
-  let route = this.subject();
-  assert.ok(route);
+  test('it exists', function(assert) {
+    let route = this.owner.lookup('route:no-help');
+    assert.ok(route);
+  });
 });
